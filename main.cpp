153d29efebcbd1fb3f203c5c7f6d67ad4052370b
Mokhtari Mohammed El Amine <37108394+amine0110@users.noreply.github.com>
2018-12-28 16:22:02 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+	cout << "Hello world !" << endl;
+	cout << "Amine " << endl;
+	cout << "something 1 " << "\n" <<"something 2" << endl; 
+	
+	return 0;
+}
